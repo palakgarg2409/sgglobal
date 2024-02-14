2ffa997909c90cd272562233f6adaa9f8c148973
Palak Garg <palakgarg2409@gmail.com>
2024-02-14 17:28:03 +0100

    first

--- js/navbar.js
+++ js/navbar.js
@@ -1,12 +1,24 @@
 let scrollPos = window.scrollY; 
+const navbar = document.getElementById("mainNav");
+const navbarStyle = window.getComputedStyle(navbar);
+const navbarWidthWithPadding = navbarStyle.getPropertyValue("height");
+console.log(typeof(navbarWidthWithPadding));
+console.log(navbarWidthWithPadding);
+
+const screenWidth = window.screen.width;
+console.log("Screen width:", screenWidth);
 
 window.addEventListener('scroll', function(){
     const newScrollPos = window.scrollY;
 
-    if(scrollPos > newScrollPos){
-        document.querySelector('.navbar').style.top="0px";
+    if(screenWidth > 700){
+        if(scrollPos > newScrollPos){
+            document.querySelector('.navbar').style.top="0px";
+        }else{
+            document.querySelector('.navbar').style.top= "-" + navbarWidthWithPadding;
+        }
     }else{
-        document.querySelector('.navbar').style.top="-100px";
+        document.querySelector('.navbar').style.top="0px";
     }
 
     scrollPos = newScrollPos;
